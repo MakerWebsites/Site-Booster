a7869a56c579a652e63f6a9d803ac95dae68ca9e
MakerWebsites <makerwebsitess@gmail.com>
2025-04-16 22:16:11 -0300

    Add files via upload

--- carregarPaginas.js
+++ carregarPaginas.js
@@ -3,14 +3,12 @@
 function entrarEmContato() {
 
 
-    let alertaUsuario = "Você será direcionado para um dos nossos atendentes.";
-    alert(alertaUsuario);
     let numero = "+5511968559541";
-    let mensagem = "Olá";
+    let mensagem = "Olá, estou entrando em contato atráves do site *www.sitebooster.com.br* tenho interesse em saber mais sobre os serviços oferecidos.";
 
     let url = `https://wa.me/${numero}/?text=${mensagem}`;
-    let urlPaginaConstrucao = "paginaEmConstrucao.html";
-    window.open(urlPaginaConstrucao);
+    let urlDirecionamento = "paginaEmConstrucao.html";
+    window.open(url);
 
 }
 
